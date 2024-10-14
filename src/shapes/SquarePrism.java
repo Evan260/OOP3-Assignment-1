064f87c75e48742608f82de4c14fd3732546e088
aulakh-savreet <147961994+aulakh-savreet@users.noreply.github.com>
2024-10-13 21:26:36 -0600

    done

--- src/shapes/SquarePrism.java
+++ src/shapes/SquarePrism.java
@@ -1,6 +1,22 @@
 package shapes;
 
-public class SquarePrism
-{
+/
+public class SquarePrism extends Shape {
+    private double side;
 
+    
+    public SquarePrism(double height, double side) {
+        super(height);
+        this.side = side;
+    }
+
+    @Override
+    public double calcVolume() {
+        return calcBaseArea() * height;  
+    }
+
+    @Override
+    public double calcBaseArea() {
+        return Math.pow(side, 2); 
+    }
 }
--- src/shapes/TriangularPrism.java
+++ src/shapes/TriangularPrism.java
@@ -1,6 +1,22 @@
 package shapes;
 
-public class TriangularPrism
-{
+public class TriangularPrism extends Shape {
+    private double triangleBase;
+    private double triangleHeight;
 
+    public TriangularPrism(double height, double triangleBase, double triangleHeight) {
+        super(height);
+        this.triangleBase = triangleBase;
+        this.triangleHeight = triangleHeight;
+    }
+
+    @Override
+    public double calcVolume() {
+        return calcBaseArea() * height;  
+    }
+
+    @Override
+    public double calcBaseArea() {
+        return 0.5 * triangleBase * triangleHeight; 
+    }
 }
